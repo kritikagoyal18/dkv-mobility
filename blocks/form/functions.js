a140c4cd92951178cb9114cc8c5ce7bedff13f3d
pankaj2186 <pansingh@adobe.com>
2025-03-14 11:22:38 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -6,57 +6,39 @@
  * @return {string}
  */
 function getFullName(firstname, lastname) {
-    const globals= arguments[arguments.length-1];    
-    firstname=(firstname==null)?"":firstname;
-    lastname=(lastname==null)?"":lastname;
-    console.log(globals);
-    return firstname.concat(' ').concat(lastname);
+  return `${firstname} ${lastname}`.trim();
 }
 
 /**
  * Calculate the number of days between two dates.
  * @param {*} endDate
  * @param {*} startDate
- * @return {number} returns the number of days between two dates
+ * @param {scope} globals
+ * @returns {number} returns the number of days between two dates
  */
-function days(endDate, startDate) {
-  const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
-  const end = typeof endDate === 'string' ? new Date(endDate) : endDate;
+function days(endDate, startDate, globals) {
+  const start = typeof startDate === 'string' ? globals.toNumber(startDate) : startDate;
+  const end = typeof endDate === 'string' ? globals.toNumber(endDate) : endDate;
 
   // return zero if dates are valid
-  if (Number.isNaN(start.getTime()) || Number.isNaN(end.getTime())) {
+  if (Number.isNaN(start) || Number.isNaN(end)) {
     return 0;
   }
-
-  const diffInMs = Math.abs(end.getTime() - start.getTime());
-  return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+  return end - start;
 }
 
 /**
- * Makes a REST call
- * @name submitToRestEndpoint Submits the request to REST endpoint
- * @return {string}
+ * Set the maximum date for a date field.
+ * @param {scope} globals
  */
-function submitToRestEndpoint(){
-    console.log("hola!");
-    let result = "";
-    $.ajax({
-        url: '/content/dkvm-sign-endpoint',
-        type: 'POST',
-        //data: JSON.stringify({"actionNr": "8504"}),
-        success: function(response) {
-            alert("Thank you for submitting the form. We'll get back to you soon!");
-            console.log("success from post call");
-            result = "success";
-        },
-        error: function(xhr, status, error) {
-            alert("Thank you for submitting the form. We'll get back to you soon!");
-            console.error("Error: " + error);
-            result = "error";
-        }
-    });
-    return result;
+function setMaximumDate(globals) {
+  const date1 = new Date();
+  // get a date 18 years and 1 day ago
+  const minimum = new Date(date1.getFullYear() - 18, date1.getMonth(), date1.getDate() - 1);
+  // convert to YYYY-MM-DD
+  const stringDate = minimum.toISOString().split('T')[0];
+  globals.functions.setProperty(globals.field, { maximum: stringDate });
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days, submitToRestEndpoint };
+export { getFullName, days, setMaximumDate };
